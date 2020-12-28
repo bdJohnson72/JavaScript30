948978760f6cf934ac607a4b131eb126616916b7
Brooks Johnson <brooksjohnson157@gmail.com>
2020-12-28 08:21:35 -0500

    day 1

---
+++ 01 - JavaScript Drum Kit/drumkit.js
@@ -0,0 +1,21 @@
+
+function playSound(event){
+   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+   const key = document.querySelector(`div[data-key="${event.keyCode}"]`)
+
+   if(!audio){return}
+   audio.currentTime = 0;
+   audio.play();
+   key.classList.add('playing');
+}
+
+function removeTransition(event){
+   if(event.propertyName != 'transform'){
+      event.target.classList.remove('playing');
+   }
+}
+
+window.addEventListener('keydown', playSound);
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
